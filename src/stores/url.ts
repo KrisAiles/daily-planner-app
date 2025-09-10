e5ffda5c4179d00cea761fc11c431f1b639225f3
Kris <kris28@btinternet.com>
2025-09-10 15:36:13 +0100

    Update server url

--- src/stores/url.ts
+++ src/stores/url.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useUrlStore = defineStore('url', () => {
-    const url = ref('http://localhost:3000');
+    const url = ref('https://daily-planner-3faa0dc63243.herokuapp.com');
 
   return { url }
 });
