d5da12833dce3757c225d49719e9768b777bbfae
Kris <kris28@btinternet.com>
2025-09-14 16:38:05 +0100

    update server url

--- src/stores/url.ts
+++ src/stores/url.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useUrlStore = defineStore('url', () => {
-    const url = ref('https://server.nextbigideamedia.com');
+    const url = ref('https://server.daily-planner.uk');
 
   return { url }
 });
